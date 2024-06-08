4330fef947749631656434f92caccc4dfbf4aa8d
Vladyslav Radchenko <vladyslav.radchenko.ki.2019@lpnu.ua>
2024-06-08 13:41:11 +0300

    [Hw3] Training Service Added

--- airflow/dags/data_transfer_dag.py
+++ airflow/dags/data_transfer_dag.py
@@ -1,61 +1,66 @@
-from minio import Minio, S3Error
-from pathlib import Path
+import json
+
+from azure.storage.blob import BlobServiceClient
+from azure.core.exceptions import ResourceExistsError
 from datetime import datetime, timedelta
 
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from pathlib import Path
+
+config_path = '/opt/airflow/config/secrets.json'
+
+# Load the JSON file
+with open(config_path, 'r') as file:
+    config = json.load(file)
+
+ACCOUNT_URL = config["account_url"]
+SAS_TOKEN = config["sas_token"]
+CONTAINER_NAME = config["container_name"]
+DATA_DIRECTORY = config["data_directory"]
+
+# Create a BlobServiceClient
+blob_service_client = BlobServiceClient(account_url=ACCOUNT_URL, credential=SAS_TOKEN)
 
 
-BUCKET_NAME = 'train-data'
-
-# Initialize MinIO client
-client = Minio('minio:9000',
-               access_key='minio',
-               secret_key='miniosecret',
-               secure=False)
-
-
-def create_bucket_if_not_exists(minio_client, bucket_name):
+def create_container_if_not_exists(blob_service_client, CONTAINER_NAME):
     """
-    Ensure the bucket exists in MinIO.
+    Ensure the container exists in Azure Blob Storage.
     """
     try:
-        if not minio_client.bucket_exists(bucket_name):
-            minio_client.make_bucket(bucket_name)
-            print(f'Bucket {bucket_name} created.')
-        else:
-            print(f'Bucket {bucket_name} already exists.')
-    except S3Error as e:
-        print(f"Failed to create or check bucket {bucket_name}: {e}")
+        container_client = blob_service_client.get_container_client(CONTAINER_NAME)
+        container_client.create_container()
+        print(f'Container {CONTAINER_NAME} created.')
+    except ResourceExistsError:
+        print(f'Container {CONTAINER_NAME} already exists.')
 
 
-def file_exists_in_bucket(minio_client, object_name):
+def file_exists_in_container(container_client, blob_name):
     """
-    Function to check if a file exists in the bucket
+    Function to check if a file exists in the container.
     """
-    objects = minio_client.list_objects(BUCKET_NAME)
-    for obj in objects:
-        if obj.object_name == object_name:
-            return True
-    return False
+    blob_client = container_client.get_blob_client(blob_name)
+    return blob_client.exists()
 
 
-def transfer_to_minio():
+def transfer_to_azure_blob():
     """
-    Function that transfers data to the MinIO bucket.
+    Function that transfers data to the Azure Blob Storage container.
     """
-    create_bucket_if_not_exists(client, BUCKET_NAME)
+    create_container_if_not_exists(blob_service_client, CONTAINER_NAME)
+    container_client = blob_service_client.get_container_client(CONTAINER_NAME)
 
     # Loop through each file in the directory
-    for file_path in Path('data/').iterdir():
+    for file_path in Path(DATA_DIRECTORY).iterdir():
         if file_path.is_file():
-            # Check if the file already exists in the bucket
-            if not file_exists_in_bucket(client, file_path.name):
+            # Check if the file already exists in the container
+            if not file_exists_in_container(container_client, file_path.name):
                 # Upload the file if it does not exist
-                client.fput_object(BUCKET_NAME, object_name=file_path.name, file_path=str(file_path))
-                print(f'Uploaded {file_path.name} to {BUCKET_NAME}')
+                with open(file_path, "rb") as data:
+                    container_client.upload_blob(name=file_path.name, data=data, overwrite=True)
+                print(f'Uploaded {file_path.name} to {CONTAINER_NAME}')
             else:
-                print(f'{file_path.name} already exists in the bucket {BUCKET_NAME}.')
+                print(f'{file_path.name} already exists in the container {CONTAINER_NAME}.')
 
 
 default_args = {
@@ -69,4 +74,4 @@ default_args = {
 
 with DAG('Data_Transfer_Dag', default_args=default_args, schedule_interval="@daily", tags=["MLOPs", "Hw2"]):
 
-    PythonOperator(task_id='Data_Transfer_From_Local_To_MinIO', python_callable=transfer_to_minio)
+    PythonOperator(task_id='Data_Transfer_From_Local_To_Azure', python_callable=transfer_to_azure_blob)
